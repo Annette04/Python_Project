aa0200507200c06636b89b4e7b8d5cc08fda8580
Anne <128488854+Annette04@users.noreply.github.com>
2024-01-15 20:49:48 +0500

    No exist the limit of perfectly

--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
     path('lastVac.html', lastVac_page),
     path('about.html', about_page)
 ]
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
